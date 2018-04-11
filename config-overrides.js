1184eb46650dcd9a7ccff54dae60da58a1873fc4
wapznw <wapznw@gmail.com>
2018-04-11 23:17:27 +0800

    add pages

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const {injectBabelPlugin} = require('react-app-rewired');
-
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}], config);
-  if ('production' === env) config.devtool = 'none';
-  return config;
-};
